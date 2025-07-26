ce16ac51d579553f5ec4abdc36b665241665842b
Caswall Engelsman <mail@cengelsman.com>
2025-07-26 16:41:53 +0200

    Change the parser interface

--- src/components/parser.c
+++ src/components/parser.c
@@ -7,6 +7,7 @@
 #include <utils/ignore_space.h>
 #include <utils/split_str_array.h>
 #include <utils/xstrchr.h>
+#include <utils/xstrncpy.h>
 #include <json.h>
 
 static json_type_t
@@ -18,12 +19,19 @@ json_value_parse(const char* s, const char** end, void** value) {
 
     switch(*s){
         case '{': {
+            json_t* json;
             char* e = other_end(s, "{}");
             if (!e) return JSON_PARSE_ERROR;
 
             type = JSON_OBJECT;
 
-            v = json_value_ref(json_parse(s, e - s), type);
+            json = malloc(sizeof(json_t));
+            if (!json) return JSON_MEMORY_ALLOC_ERROR;
+
+            if (json_parse(json, s, e - s))
+                return JSON_PARSE_ERROR;
+
+            v = json_value_ref(json, type);
             if (!v) return JSON_MEMORY_ALLOC_ERROR;
 
             s = e + 1;
@@ -154,21 +162,19 @@ json_value_parse(const char* s, const char** end, void** value) {
     return type;
 }
 
-json_t*
-json_parse(const char* start, size_t len) {
-    const char* s = start, *e = start + len, *value_end = NULL;
+int
+json_parse(json_t* json, const char* start, size_t len) {
     json_type_t ret;
-    json_t* json;
-
-    json = malloc(sizeof(json_t));
-    if (!json) return NULL;
+    size_t key_start = 0, value_start = 0;
+    char* ptr;
+    char key[JSON_KEY_LIMIT] = {0};
+    const char* s = start, *e = start + len, *value_end = NULL;
 
     if (json_init(json, JSON_OBJECT))
-        return NULL;
+        return 2;
 
     while ( (s = xstrchr(s, e, '"')) && s < e ) {
         char* end_quote;
-        char key[JSON_KEY_LIMIT] = {0};
         void* value = NULL;
 
         end_quote = (char*)s;
@@ -176,19 +182,23 @@ json_parse(const char* start, size_t len) {
             end_quote = xstrchr(end_quote + 1, e, '"');
         } while (end_quote[-1] == '\\');
 
-        strncpy(key, s + 1, end_quote - (s + 1));
+        key_start = s - start + 1;
+        ptr = xstrncpy(key, s + 1, end_quote - (s + 1));
+        *ptr = 0;
+
         s = end_quote;
         s = ignore_space(s + 1);
         if (*s != ':')
-            goto failed;
+            goto failed_key_parse;
 
         s = ignore_space(s + 1);
 
+        value_start = s - start + 1;
         ret = json_value_parse(s, &value_end, &value);
         switch (ret) {
             case JSON_PARSE_ERROR:
             case JSON_MEMORY_ALLOC_ERROR:
-                goto failed;
+                goto failed_value_parse;
             default:
             break;
         }
@@ -198,10 +208,55 @@ json_parse(const char* start, size_t len) {
         s = value_end;
     }
 
-    return json;
+    return 0;
+
+failed_key_parse:
+    fprintf(stderr, "Failed to parse at character: %ld\n", key_start);
+    json_free(json);
+    return 1;
+
+failed_value_parse:
+    fprintf(stderr, "Failed to parse at character: %ld\n", value_start);
+    json_free(json);
+    return 1;
 
 failed:
     json_free(json);
     free(json);
-    return NULL;
+    return 2;
+}
+
+int
+json_parse_file(json_t* json, const char* file_path) {
+    FILE* f;
+    char* str;
+    size_t size = 0, ret;
+
+    if (!(f = fopen(file_path, "r"))) return 1;
+
+    fseek(f, 0, SEEK_END);
+    size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    str = malloc(sizeof(char) * (size + 1));
+    if(!str) goto failed_after_fopen;
+
+    ret = fread(str, sizeof(char), size, f);
+    str[ret] = 0;
+
+    ret = json_parse(json, str, ret);
+    if(ret) goto failed_parsing;
+
+    free(str);
+    fclose(f);
+    return 0;
+
+failed_after_fopen:
+    fclose(f);
+    return 2;
+
+failed_parsing:
+    fclose(f);
+    free(str);
+    return ret;
 }
