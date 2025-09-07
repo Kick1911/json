e4b0b78e12427af186c0c73d03bc254fa41697f0
Caswall Engelsman <mail@cengelsman.com>
2025-09-07 20:43:07 +0200

    Fix tool

--- src/json.c
+++ src/json.c
@@ -3,19 +3,19 @@
 #include <malloc.h>
 
 int main(int argc, char** argv){
+    int ret;
     char* res;
-    json_t* json = NULL;
+    json_t json;
 
     if (argc < 2) { fprintf(stderr, "Needs file path argument.\n"); return 1; }
 
-    json = json_parse_file(argv[1]);
-    if (!json) { perror("JSON parse error"); return 2; }
+    ret = json_parse_file(&json, argv[1]);
+    if (ret) { perror("JSON parse error"); return ret; }
 
-    res = json_dump(json, 1);
+    res = json_dump(&json, 0);
     printf("%s\n", res);
 
-    json_free(json);
-    free(json);
+    json_free(&json);
     free(res);
     return 0;
 }
